609106ef25016d445e6b4772089a1ef82d837d30
Guilherme Zobel <guilherme.zobel@gmail.com>
2023-07-27 13:51:12 -0300

    feat: Criando DataGrid da listagem de categoria

--- src/features/categories/CategoryList.tsx
+++ src/features/categories/CategoryList.tsx
@@ -1,9 +1,47 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { useAppSelector } from "../../app/hooks";
+import { selectCategories } from "./categorySlice";
+import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
 
-export const CategoryList = () => (
-    <Box>
-      <Typography variant="h3" component="h1">
-        CategoryList Page
-      </Typography>
+export const CategoryList = () => {
+  const categories = useAppSelector(selectCategories)
+
+  const rows: GridRowsProp = categories.map((category) => ({
+    id: category.id,
+    name: category.name, 
+    description: category.description,
+  }))
+  
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 150 },
+    { field: 'name', headerName: 'Name 2', width: 150 },
+    { field: 'description', headerName: 'Description', width: 150 },
+  ];
+
+  return (
+    <Box maxWidth="lg" sx={{ mt: 4, mb: 4}}>
+      <Box display="flex" justifyContent="flex-end">
+        <Button
+          variant="contained"
+          color="secondary"
+          component={Link}
+          to="/categories/create"
+          style={{ marginBottom: "1rem"}}
+        >
+          New Category
+        </Button>
+      </Box>
+
+        {/* {categories.map((category) => (
+          <Typography key={category.id}>{category.name}</Typography>
+        ))} */}
+      <div style={{ height: 300, width: "100%"}}>
+        <DataGrid
+          pageSizeOptions={[2, 20, 50, 100]}
+          rows={rows}
+          columns={columns}/>
+      </div>
     </Box>
-  );
+  )
+};
--- src/features/categories/categorySlice.ts
+++ src/features/categories/categorySlice.ts
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
 
 interface Category {
   id: string;
@@ -37,5 +38,8 @@ const categoriesSlice = createSlice({
     deleteCategory(state, acrtion) {},
   },
 });
+
+export const selectCategories = (state: RootState) => state.categories;
+
 
 export default categoriesSlice.reducer;
