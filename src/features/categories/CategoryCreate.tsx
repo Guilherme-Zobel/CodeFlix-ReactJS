551506a9007add42f2305f3a59170203b8f9c25a
Guilherme Zobel <guilherme.zobel@gmail.com>
2023-08-01 16:31:12 -0300

    feat: Criando Página de criação de categoria

--- src/features/categories/CategoryCreate.tsx
+++ src/features/categories/CategoryCreate.tsx
@@ -1,9 +1,42 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Paper, Typography } from "@mui/material";
+import { useState } from "react";
+import { Category } from "./categorySlice";
+import { CategoryForm } from "./components/CategoryForm";
 
-export const CategoryCreate = () => (
-    <Box>
-      <Typography variant="h3" component="h1">
-      CategoryCreate Page
-      </Typography>
-    </Box>
+export const CategoryCreate = () => {
+  const [isdisabled, setIsdisabled] = useState(false);
+  const [category, setCategory] = useState<Category>({
+      id: "",
+      name: "",
+      description: "",
+      is_active: false,
+      created_at: "",
+      deleted_at: "",
+      updated_at: "",
+    }
   );
+
+  const handleChange = (e: any) => {};
+
+  const handleToggle = (e: any) => {};
+
+  return (
+    <Box>
+      <Paper>
+        <Box p={2}>
+          <Box mb={2}>
+            <Typography variant="h4">Create Category</Typography>
+          </Box>
+        </Box>
+        <CategoryForm
+          category={category}
+          isdisabled={isdisabled}
+          isLoading={false}
+          onSubmit={() => {}}
+          handleChange={handleChange}
+          handleToggle={handleToggle}
+        />
+      </Paper>
+    </Box>
+  )
+};
--- src/features/categories/CategoryEdit.tsx
+++ src/features/categories/CategoryEdit.tsx
@@ -1,8 +1,9 @@
-import { Box, Button, FormControl, FormControlLabel, FormGroup, Grid, Paper, Switch, TextField, Typography } from "@mui/material";
-import { Link, useParams } from "react-router-dom";
+import { Box, Paper, Typography } from "@mui/material";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 import { useAppSelector } from "../../app/hooks";
 import { selectCategoryById } from "./categorySlice";
-import { useState } from "react";
+import { CategoryForm } from "./components/CategoryForm";
 
 export const CategoryEdit = () => {
   const id = useParams().id || "";
@@ -18,76 +19,17 @@ export const CategoryEdit = () => {
       <Paper>
         <Box p={2}>
           <Box mb={2}>
-            <Typography variant="h3" component="h1">
-              CategoryEdit Page
-            </Typography>
+            <Typography variant="h3">Category Edit</Typography>
           </Box>
         </Box>
-
-        <Box p={2}>
-          <form>
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <FormControl fullWidth>
-                  <TextField
-                    required
-                    name="name"
-                    label="name"
-                    value={category.name}
-                    disabled={isdisabled}
-                    onChange={handleChange}
-                  />
-                </FormControl>
-              </Grid>
-              <Grid item xs={12}>
-                <FormControl fullWidth>
-                  <TextField
-                    required
-                    name="description"
-                    label="description"
-                    disabled={isdisabled}
-                    onChange={handleChange}
-                    value={category.description}
-                  />
-                <FormControl/>
-                </FormControl>
-              </Grid>
-              <Grid item xs={12}>
-                <FormGroup>
-                  <FormControlLabel
-                    control={
-                      <Switch
-                        name="is_active"
-                        color="secondary"
-                        onChange={handleToggle}
-                        checked={category.is_active}
-                        inputProps={{ "aria-label" : "controlled"}}
-                      />
-                    }
-                    label="Active"
-                  />
-                </FormGroup>
-              </Grid>
-              <Grid item xs={12}>
-                <Box display="flex" gap={2}>
-                  <Button variant="contained" component={Link} to="/categories">
-                    Back
-                  </Button>
-
-                  <Button
-                    type="submit"
-                    variant="contained"
-                    color="secondary"
-                    disabled={isdisabled}
-                  >
-                    Save
-                  </Button>
-                </Box>
-              </Grid>
-            </Grid>
-          </form>
-        </Box>
-
+        <CategoryForm
+          category={category}
+          isdisabled={isdisabled}
+          isLoading={false}
+          onSubmit={() => {}}
+          handleChange={handleChange}
+          handleToggle={handleToggle}
+        />
       </Paper>
     </Box>
   )
